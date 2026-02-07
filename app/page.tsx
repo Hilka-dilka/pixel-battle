888db906f8f3ba110437905ff55289657cf6fc94
Hilka-dilka <sapot1151@gmail.com>
2026-02-07 21:19:37 +1000

    final-version-2.3

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
   const dragStartPosRef = useRef<{x: number, y: number} | null>(null);
   const isClickActionRef = useRef<boolean>(true);
   const sizeX = 180;
-  const sizeY = 90;
+  const sizeY = 180;
   const pixelScale = 10;
 
   const drawCanvas = () => {
